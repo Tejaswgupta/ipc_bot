9e86ca8b7225ba7f02b0c656a4f1f4811d93e910
Tejasw Gupta <guptatejasw2002@gmail.com>
2024-01-01 02:45:26 +0530

    Updates

--- app.py
+++ app.py
@@ -57,87 +57,76 @@ async def on_chat_start():
         api_key="xVLVqK38Xf5HYBM4GaKrGy0Csorj1Gc9YJdqn6DARiXD6ES5SuMrfA",
     )
 
-    # conn = await asyncpg.connect(host="legalscraperserver.postgres.database.azure.com",
-    #                              database="postgres",
-    #                              user="tejasw",
-    #                              password="Password1234",
-    #                              port=5432,)
-    # row = await conn.fetch(
-    #     '''SELECT sections, act_name, text
-    # FROM acts
-    # WHERE sections IS NOT NULL
-    # AND(act_name LIKE '%The Indian Penal Code, 1860%' OR act_name LIKE '%The Code of Criminal Procedure, 1973%' OR act_name LIKE '%Indian Evidence Act%');
-    # ''')
+    conn = await asyncpg.connect(host="legalscraperserver.postgres.database.azure.com",
+                                 database="postgres",
+                                 user="tejasw",
+                                 password="Password1234",
+                                 port=5432,)
+    row = await conn.fetch(
+        '''SELECT sections, act_name, text
+    FROM acts
+    WHERE sections IS NOT NULL
+    AND(act_name LIKE '%The Indian Penal Code, 1860%' OR act_name LIKE '%The Code of Criminal Procedure, 1973%' OR act_name LIKE '%Indian Evidence Act%');
+    ''')
 
-    # def remove_between_periods(sentence):
-    #     # Define a regular expression pattern to match text between two periods
-    #     pattern = r'\.(.*?)\.'
+    def remove_between_periods(sentence):
+        # Define a regular expression pattern to match text between two periods
+        pattern = r'\.(.*?)\.'
 
-    #     # Use re.sub to replace the matched substring with an empty string
-    #     modified_sentence = re.sub(pattern, '.', sentence)
+        # Use re.sub to replace the matched substring with an empty string
+        modified_sentence = re.sub(pattern, '.', sentence)
+        modified_sentence = re.sub(r'\.', '', modified_sentence)
 
-    #     return modified_sentence
+        return modified_sentence
 
-    # def preprocess_text(text):
-    #     text = re.sub(r'(?:\n\s*)+', '\n', text)
-    #     text = re.sub(r'\*', '', text)
+    def preprocess_text(text):
+        text = re.sub(r'(?:\n\s*)+', '\n', text)
+        text = re.sub(r'\*', '', text)
 
-    #     return text
+        return text
 
-    # for act in row:
-    #     for section in act['sections']:
-    #         json_data = json.loads(section)
-    #         # if 'omitted.' in json_data['section_name'].lower():
-    #             # continue
-    #         json_data['section_name'] = f"{remove_between_periods(json_data['section_name'])} of {act['act_name'].replace(',','')}"
-    #         d = json_data['section_name'] + ' : ' + json_data['text']
-    #         data.append({
-    #             'name': act['act_name'],
-    #             'content':preprocess_text(d)
-    #         })
+    def process(act):
+        data = []
+        for section in act['sections']:
+            json_data = json.loads(section)
+            # if 'omitted.' in json_data['section_name'].lower():
+            # continue
+            json_data['section_name'] = f"{remove_between_periods(json_data['section_name'])} of {act['act_name'].replace(',','')}"
+            d = json_data['section_name'] + ' : ' + json_data['text']
+            data.append({
+                'name': json_data['section_name'],
+                'content': preprocess_text(d),
+                # 'section_name': json_data['section_name'],
+            })
+        return data
 
-    # def process(act):
-    #     data = []
-    #     for section in act['sections']:
-    #         json_data = json.loads(section)
-    #         # if 'omitted.' in json_data['section_name'].lower():
-    #         # continue
-    #         json_data['section_name'] = f"{remove_between_periods(json_data['section_name'])} of {act['act_name'].replace(',','')}"
-    #         d = json_data['section_name'] + ' : ' + json_data['text']
-    #         data.append({
-    #             'name': json_data['section_name'],
-    #             'content': preprocess_text(d),
-    #             # 'section_name': json_data['section_name'],
-    #         })
-    #     return data
+    ipc_loader = [Document(page_content=s['content'], metadata={
+        'name': s['name']}) for s in process(row[0])]
+    iea_loader = [Document(page_content=s['content'], metadata={
+        'name': s['name']}) for s in process(row[1])]
+    crpc_loader = [Document(page_content=s['content'], metadata={
+                            'name': s['name']}) for s in process(row[2])]
 
-    # ipc_loader = [Document(page_content=s['content'], metadata={
-    #     'name': s['name']}) for s in process(row[0])]
-    # iea_loader = [Document(page_content=s['content'], metadata={
-    #     'name': s['name']}) for s in process(row[1])]
-    # crpc_loader = [Document(page_content=s['content'], metadata={
-    #                         'name': s['name']}) for s in process(row[2])]
+    with open('bsa.json', 'r') as bsa:
+        bsa_loader = [
+            Document(
+                page_content=f"{s['section_name']} of Bharatiya Sakshya Adhiniyam(BSA): {s['content']}",
+                metadata={
+                    'name': f"{s['section_name']} of Bharatiya Sakshya Adhiniyam(BSA)"}) for s in json.load(bsa)]
 
-    # with open('bsa.json', 'r') as bsa:
-    #     bsa_loader = [
-    #         Document(
-    #             page_content=f"{s['section_name']} of Bharatiya Sakshya Adhiniyam(BSA): {s['content']}",
-    #             metadata={
-    #                 'name': f"{s['section_name']} of Bharatiya Sakshya Adhiniyam(BSA)"}) for s in json.load(bsa)]
+    with open('bns.json', 'r') as bns:
+        bns_loader = [
+            Document(
+                page_content=f"{s['section_name']} of Bharatiya Nyaya Sanhita(BNS): {s['content']}",
+                metadata={
+                    'name': f"{s['section_name']} of Bharatiya Nyaya Sanhita(BNS)"}) for s in json.load(bns)]
 
-    # with open('bns.json', 'r') as bns:
-    #     bns_loader = [
-    #         Document(
-    #             page_content=f"{s['section_name']} of Bharatiya Nyaya Sanhita(BNS): {s['content']}",
-    #             metadata={
-    #                 'name': f"{s['section_name']} of Bharatiya Nyaya Sanhita(BNS)"}) for s in json.load(bns)]
-
-    # with open('bnss.json', 'r') as bnss:
-    #     bnss_loader = [
-    #         Document(
-    #             page_content=f"{s['section_name']} of Bharatiya Nagarik Suraksha Sanhita(BNSS): {s['content']}",
-    #             metadata={
-    #                 'name': f"{s['section_name']} of Bharatiya Nagarik Suraksha Sanhita(BNSS)"}) for s in json.load(bnss)]
+    with open('bnss.json', 'r') as bnss:
+        bnss_loader = [
+            Document(
+                page_content=f"{s['section_name']} of Bharatiya Nagarik Suraksha Sanhita(BNSS): {s['content']}",
+                metadata={
+                    'name': f"{s['section_name']} of Bharatiya Nagarik Suraksha Sanhita(BNSS)"}) for s in json.load(bnss)]
 
     model_name = "BAAI/bge-base-en-v1.5"
     encode_kwargs = {'normalize_embeddings': True}
@@ -204,54 +193,86 @@ You have access to the following tools:
     )
 
     mistral_llm = ChatOpenAI(openai_api_base='http://20.124.240.6:8083/v1',
-                             openai_api_key='none', model='TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ', temperature=0.7)
+                             openai_api_key='none',
+                             model='TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ',
+                             temperature=0.7)
 
     @tool('search_ipc')
     def search_ipc(query: str) -> str:
         """Query tool to search and return the relevant Indian Penal Code (IPC) section name and description."""
+        bm25_retriever = BM25Retriever.from_documents(ipc_loader)
+        bm25_retriever.k = 4
         retriever = ipc_db.as_retriever(
-            search_type='similarity', search_kwargs={"k": 2})
-        res = retriever.get_relevant_documents(query)
+            search_type='similarity', search_kwargs={"k": 4})
+        ensemble_retriever = EnsembleRetriever(
+            retrievers=[bm25_retriever, retriever], weights=[0.5, 0.5]
+        )
+        res = ensemble_retriever.get_relevant_documents(query)
         return res
 
     @tool('search_iea')
     def search_iea(query: str) -> str:
         """Query tool to search and return the relevant The Indian Evidence Act (IEA) section name and description."""
+        bm25_retriever = BM25Retriever.from_documents(iea_loader)
+        bm25_retriever.k = 4
         retriever = iea_db.as_retriever(
             search_type='similarity', search_kwargs={"k": 4})
-        res = retriever.get_relevant_documents(query)
+        ensemble_retriever = EnsembleRetriever(
+            retrievers=[bm25_retriever, retriever], weights=[0.5, 0.5]
+        )
+        res = ensemble_retriever.get_relevant_documents(query)
         return res
 
     @tool('search_crpc')
     def search_crpc(query: str) -> str:
         """Query tool to search and return the relevant Code of Criminal Procedure (CRPC) section name and description."""
+        bm25_retriever = BM25Retriever.from_documents(crpc_loader)
+        bm25_retriever.k = 4
         retriever = crpc_db.as_retriever(
             search_type='similarity', search_kwargs={"k": 4})
-        res = retriever.get_relevant_documents(query)
+        ensemble_retriever = EnsembleRetriever(
+            retrievers=[bm25_retriever, retriever], weights=[0.5, 0.5]
+        )
+        res = ensemble_retriever.get_relevant_documents(query)
         return res
 
     @tool('search_bsa')
     def search_bsa(query: str) -> str:
         """Query tool to search and return the relevant Bharatiya Sakshya Adhiniyam(BSA) section name and description."""
+        bm25_retriever = BM25Retriever.from_documents(bsa_loader)
+        bm25_retriever.k = 4
         retriever = bsa_db.as_retriever(
             search_type='similarity', search_kwargs={"k": 4})
-        res = retriever.get_relevant_documents(query)
+        ensemble_retriever = EnsembleRetriever(
+            retrievers=[bm25_retriever, retriever], weights=[0.5, 0.5]
+        )
+        res = ensemble_retriever.get_relevant_documents(query)
         return res
 
     @tool('search_bns')
     def search_bns(query: str) -> str:
         """Query tool to search and return the relevant Bharatiya Nyaya Sanhita (BNS) section name and description."""
+        bm25_retriever = BM25Retriever.from_documents(bns_loader)
+        bm25_retriever.k = 4
         retriever = bns_db.as_retriever(
             search_type='similarity', search_kwargs={"k": 4})
-        res = retriever.get_relevant_documents(query)
+        ensemble_retriever = EnsembleRetriever(
+            retrievers=[bm25_retriever, retriever], weights=[0.5, 0.5]
+        )
+        res = ensemble_retriever.get_relevant_documents(query)
         return res
 
     @tool('search_bnss')
     def search_bnss(query: str) -> str:
         """Query tool to search and return the relevant Bharatiya Nagarik Suraksha Sanhita Sanhita(BNSS) section name and description."""
+        bm25_retriever = BM25Retriever.from_documents(bnss_loader)
+        bm25_retriever.k = 4
         retriever = bnss_db.as_retriever(
             search_type='similarity', search_kwargs={"k": 4})
-        res = retriever.get_relevant_documents(query)
+        ensemble_retriever = EnsembleRetriever(
+            retrievers=[bm25_retriever, retriever], weights=[0.5, 0.5]
+        )
+        res = ensemble_retriever.get_relevant_documents(query)
         return res
 
     template = """This is a conversation between a human and a bot:
@@ -319,6 +340,5 @@ You have access to the following tools:
 @cl.on_message
 async def main(message: cl.Message):
     agent = cl.user_session.get("agent")  # type: AgentExecutor
-    # answer_prefix_tokens = ["FINAL", "ANSWER"]
-    cb = cl.AsyncLangchainCallbackHandler(stream_final_answer=True)
+    cb = cl.LangchainCallbackHandler(stream_final_answer=True)
     await cl.make_async(agent.run)(message.content, callbacks=[cb])
